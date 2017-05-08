8a8bac26097a4fc90cf33f0160af52f1dca70940
Alexander Reitzel <funtimecoding@gmail.com>
2017-05-08 17:33:58 +0200

    use echo rather than cat

--- bin/generate-key.sh
+++ bin/generate-key.sh
@@ -1,13 +1,11 @@
 #!/bin/sh -e
 
-cat > settings.txt <<EOF
-Key-Type: default
+echo "Key-Type: default
 Subkey-Type: default
 Name-Real: Example User
 Name-Comment: Example signature key
 Name-Email: example@example.org
 Expire-Date: 1y
-Passphrase: example
-EOF
+Passphrase: example" > settings.txt
 
 gpg2 --batch --gen-key settings.txt
