0b3d8521d561a64051768385042ccf8133ad2090
Alexander Reitzel <funtimecoding@gmail.com>
2018-05-22 10:43:44 +0200

    update job configuration

--- script/job-configuration/generate.sh
+++ script/job-configuration/generate.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 
 # shellcheck disable=SC2016
-jjm --locator ssh://git@vc.shiin.org:2222/devops/gnu-privacy-guard-tools.git --build-command script/build.sh --checkstyle 'build/log/checkstyle-*.xml' --recipients funtimecoding@gmail.com > job.xml
+jjm --locator git@github.com:FunTimeCoding/gnu-privacy-guard-tools.git --build-command script/build.sh --checkstyle 'build/log/checkstyle-*.xml' --recipients funtimecoding@gmail.com > job.xml
